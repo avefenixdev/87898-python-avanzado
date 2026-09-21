60196010e5b2962f1d00db5b4ddfc30708805465
Ave Fenix Dev <avefenixdev@gmail.com>
2026-09-21 09:52:01 -0300

    Empezando con las clase 01 - repaso

---
+++ clase-01/main.py
@@ -0,0 +1,94 @@
+print('Clase 01 - Python Avanzado')
+
+"""
+! Python es un lenguaje
+
+* Alto nivel.
+* Interpretado.
+* Dinámicamente tipado. -tipado dinámico- (Fuertemente tipado -tipado estático-)
+* Sitaxis sencilla.
+* Multiproposito -> Inicialmente se usaba para scripting
+* Gran comunidad
+
+! Nos permite hacer
+
+* Desarrollo Web
+* Ciencia de datos
+* Scripting (Administración de sistema)
+* Backend
+* Cyber Seguridad
+* Video Juegos
+* Automatizaciones
+* Inteligencia artificial
+* App de escritorio
+
+! Links útiles
+* https://www.python.org.ar/
+* https://pypi.org/
+"""
+
+"""
+! print -> Permite imprimir en terminal/consola
+"""
+
+print('Hola tarolas')
+
+print( 10 + 20 )
+
+nombre = "Maxi"
+version = 3.14
+
+print(nombre)
+print(version)
+
+""" IMPORTANTE: El programa ser interpretado. Las sentencias se ejecutan de arriba para abajo y de izquierda a derecha """
+
+""" ! VARIABLES """
+
+nombre = "Luis"; """ cadena """
+edad = 33; """ número entero """
+edad = 1.80; """ número decimal """
+
+""" Podemos en Python cambiar el valor de una variable """
+
+""" ! Reasignación """
+
+print(edad)
+edad = 22;
+print(edad);
+edad = 44;
+print(edad);
+
+contador = 0;
+contador = contador + 1
+contador = contador + 1
+
+print(contador); """ 2 """
+
+contador += 1
+
+print(contador); """ 3 """
+
+print('Entrada de datos por el usuario')
+
+nombre = input('Ingrese su nombre: ')
+
+print("Hola", nombre, "!")
+print("Hola " + nombre + '!')
+print(f"Hola {nombre}!") # <---- Está es la que vamos a usar
+print("Hola %s!" % nombre)
+
+""" Ingresamos la edad """
+
+edad_input = input('Edad:')
+print(type(edad_input)); """ cadena """
+print(edad_input) # 22
+
+""" ¿Cómo averiguamos el tipo de dato de una variable"""
+""" Con la función type() """
+
+print(""" Casteando la edad """)
+
+edad_ingresada = int(input("Edad: ")) # Casteamos la cadena a un entero
+print(type(edad_ingresada)) # número
+print(edad_ingresada)
